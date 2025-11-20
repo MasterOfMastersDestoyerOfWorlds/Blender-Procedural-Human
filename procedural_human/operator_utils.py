53bfd6a1556faba3190d2a7f93a4b45b44480c49
Andrew Wollack <andrew.wollack@gmail.com>
2025-11-20 08:41:26 -0500

    ree

--- procedural_human/operator_utils.py
+++ procedural_human/operator_utils.py
@@ -1,15 +1,14 @@
-def create_geometry_nodes_modifier(box, context, name: str):
+from bpy.types import UILayout, Context
+
+
+def create_geometry_nodes_modifier(box: UILayout, context: Context, name: str) -> None:
     snake_name = name.replace(" ", "_").lower()
-    box.label(text="Finger Operations:")
+    box.label(text=f"{name} Operations:")
     box.operator(f"mesh.procedural_{snake_name}", text=f"Create {name}")
-
     box.operator(f"mesh.procedural_realize_{snake_name}", text=f"Realize Geometry")
-
     box.separator()
-
     box.operator(f"mesh.procedural_add_armature_{snake_name}", text=f"Add Armature")
     box.operator(
         f"mesh.procedural_create_animation_{snake_name}",
         text="Create Animation",
     )
-
